62f0b321227709b3b64e0452d1157a858f62b89a
K3lKuN <kelkunsec@gmail.com>
2016-10-19 15:23:25 +0200

    refonte avec codeigniter pour du MVC et smarty

---
+++ application/third_party/Smarty/templates_c/a88626db65725af310cd03b516c62691507a6f5e_0.file.header.php.cache.php
@@ -0,0 +1,51 @@
+<?php
+/* Smarty version 3.1.30, created on 2016-10-19 09:41:55
+  from "D:\OneDrive\Developpement\www\web2\application\views\Layout\header.php" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_58073fe3a35555_12969807',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'a88626db65725af310cd03b516c62691507a6f5e' => 
+    array (
+      0 => 'D:\\OneDrive\\Developpement\\www\\web2\\application\\views\\Layout\\header.php',
+      1 => 1476870004,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_58073fe3a35555_12969807 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->compiled->nocache_hash = '817958073fe3a31031_06817351';
+?>
+<!doctype html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>Master Link</title>
+
+		<!-- Jquery -->
+		<?php echo '<script'; ?>
+ src="./res/js/jquery.js"><?php echo '</script'; ?>
+>
+		<!-- Mon fichier CSS -->
+		<link rel="stylesheet" href="./res/css/style.css">
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<?php echo '<script'; ?>
+ src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"><?php echo '</script'; ?>
+>
+
+	</head><?php }
+}
---
+++ application/views/autre.tpl
@@ -0,0 +1,4 @@
+{extends file='index.tpl'}
+{block name="article"}
+	<p>Ceci est un nouveau block</p>
+{/block}}
